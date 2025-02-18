eee8940b6869eaab79a3ac195ef7f0bba67315e4
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-02-18 20:31:54 +0530

    items display updated ✨🚀

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Testing from './components/testing'
 import LoginPage from './login/login'
 import ItemsCard from './components/itemsCard'
 import AddItem from './components/addItem'
+import RegisterPage from './assets/register/RegisterPage'
 
 function App() {
     return (
@@ -17,6 +18,8 @@ function App() {
                 <Route path="/home/*" element={<HomePage />} />
                 <Route path="/test" element={<Testing />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<h1>404 Not Found</h1>} />
             </Routes>
