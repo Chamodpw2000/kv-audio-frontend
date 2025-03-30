b1ad74ab74703830f272a72af9c986fe67b4d219
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-03-30 18:17:47 +0530

    some role base activities completed 🚀💥

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { GiHamburgerMenu } from "react-icons/gi";
 import MobileNavPannel from './mobileNavPannel';
 
-const Header = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
+const Header = ({auth}) => {
+
+  const props = useParams(auth)
+
+ 
+
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+  useEffect(() => {
+
+    setUser(JSON.parse(localStorage.getItem('user')));
+  }, [auth]);
   const [navPannelOpen, setNavPannelOpen] = useState(false);
   const token = localStorage.getItem('token');
 
@@ -14,25 +23,26 @@ const Header = () => {
      top-0 left-0 z-50'>
       <img src="/logo.png" alt="logo" className="w-[70px] h-[70px] object-cover absolute left-5" />
       <div className='w-[800px] flex items-center justify-between'>
-        <Link to="/home" className='hidden text-[20px] m-1 md:block'>Home</Link>
+        <Link to="/" className='hidden text-[20px] m-1 md:block'>Home</Link>
         <Link to="/home/contact" className='hidden text-[20px] m-1 md:block'>Contact</Link>
         <Link to="/home/gallery" className='hidden text-[20px] m-1 md:block'>Gallery</Link>
         <Link to="/home/items" className='hidden text-[20px] m-1 md:block'>Items</Link>
-        <Link to="/home/mybookings" className='hidden text-[20px] m-1 md:block'>My Bookings</Link>
+        {user?.role=="customer"&&<Link to="/home/mybookings" className='hidden text-[20px] m-1 md:block'>My Bookings</Link>}
         <GiHamburgerMenu className='absolute right-5 text-[20px] md:hidden' onClick={() => { setNavPannelOpen(true) }} />
         <MobileNavPannel isOpen={navPannelOpen} setOpen={setNavPannelOpen} />
 
-        <Link to="/home/login" className='hidden text-[20px] m-1 md:block'>Login</Link>
+       {user==null && <Link to="/home/login" className='hidden text-[20px] m-1 md:block'>Login</Link>}
+       {user==null && <Link to="/home/register" className='hidden text-[20px] m-1 md:block'>Register</Link>}
+
         {user?.role === "admin" && <Link to="/admin" className='hidden text-[20px] m-1 md:block'>Admin Dashboard</Link>}
 
-        <Link to="/home/booking" className='hidden text-[20px] font-bold m-1 absolute right-24 md:block'>
+        {user?.role==="customer" &&<Link to="/home/booking" className='hidden text-[20px] font-bold m-1 absolute right-24 md:block'>
           <FaShoppingCart />
-        </Link>
-
+        </Link>}
         {token != null && <button className='hidden md:block absolute right-5 text-[20px]' onClick={() => {
           localStorage.removeItem('token');
           localStorage.removeItem('user');
-          window.location.href = '/login';
+          window.location.href = '/';
         }}>Logout</button>}
       </div>
     </header>
--- src/pages/home/productOverview.jsx
+++ src/pages/home/productOverview.jsx
@@ -11,6 +11,8 @@ import { toast } from 'react-hot-toast';
 
 const ProductOverview = () => {
 
+    const user = JSON.parse(localStorage.getItem('user'));
+
     const params = useParams();
     const key = params.id;
     console.log("id", key);
@@ -33,7 +35,7 @@ const ProductOverview = () => {
     }, [])
 
     return (
-        <div className='w-full flex  justify-center '
+        <div className='w-full flex  justify-center py-5 '
         >
 
 
@@ -49,13 +51,20 @@ const ProductOverview = () => {
 
             {
                 loadingStatus == "loaded" && <div className='p-2 w-full  flex flex-col md:flex-row justify-center items-center bg-slate-100'>
-                    <h1 className='text-3xl font-bold text-accent text-center my-3 md:hidden'>{product.name}</h1>
-                    <div className='w-full   h-full  md:w-[49%]'>
+                    <h1 className='text-3xl font-bold text-accent text-center my-3 md:hidden pt-[50px]'>{product.name}</h1>
+                    <div className='w-full   h-full  md:w-[49%] md:hidden '>
 
                         <ImageSlider images={product.image} />
 
                     </div>
-                    <div className='w-full bg-blue-100  flex flex-col items-center md:w-[49%]  '>
+
+
+                    <div className='w-full pt-[100px]   h-full  md:w-[49%] hidden md:block '>
+
+                        <ImageSlider images={product.image} />
+
+                    </div>
+                    <div className='w-full bg-blue-100  flex flex-col items-center md:w-[49%] py-3  '>
 
                         <h1 className='text-3xl font-bold text-accent hidden md:block m-2'>{product.name}</h1>
                         <p className='text-lg text-slate-800 text-center'>Category: {product.category}</p>
@@ -67,31 +76,53 @@ const ProductOverview = () => {
                             <span className='font-medium'>Dimensions: </span>{product.dimentions}
                         </div>
 
-                        <button className='bg-accent text-white px-4 py-2 rounded-md mt-4' onClick={() => {
+                        {user?.role == "customer" && <button className='bg-accent text-white px-4 py-2 rounded-md mt-4' onClick={() => {
 
                             addToCart(product.key, 1);
                             console.log(loadCart(),
                                 toast.success("Item added to cart")
                             )
-                        }}>Add to Cart</button>
+                        }}>Add to Cart</button>}
+
+
+{user?.role ==null && <div>
+
+
+<p className='text-lg text-red-500 font-bold my-3 text-center'>
+    Login to Add Items to the cart
+</p>
+</div>}
+
+
+{user?.role =="admin" && <div>
+
+
+<p className='text-lg text-red-500 font-bold my-3 text-center'>
+    Only customer accounts can add items to the cart    
+</p>
+</div>}
 
                     </div>
 
+               
+
+                
 
 
 
-                </div>
-            }
-
-            {
-                loadingStatus == "error" && <div className='w-full h-full flex justify-center items-center'>
-
-                    <h1 className='text-2xl text-red-500'>Error loading product</h1>
 
                 </div>
             }
 
-        </div>
+{
+    loadingStatus == "error" && <div className='w-full h-full flex justify-center items-center'>
+
+        <h1 className='text-2xl text-red-500'>Error loading product</h1>
+
+    </div>
+}
+
+        </div >
     )
 }
 
